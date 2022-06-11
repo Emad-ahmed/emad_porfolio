468ec56b86e4ba2e7feb1b96a8223897e9f8c42c
Emad-ahmed <amadahmed1234678@gmail.com>
2022-06-10 19:15:56 -0700

    changes

--- emadapp/views.py
+++ emadapp/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
+
 
 class HomeView(View):
     def get(self, request):
+        return render(request, "home.html")
+
+    def post(self, request):
+        to_name = request.POST.get('name')
+        email = request.POST.get('sender')
+        message = request.POST.get('message')
+
+        content = f"Hello \nYour Registration Has Been Successfull.\nPay 500 For The Form Fee and Use This  Code As A Refernce Number Thanks For Registration.Use This Link For Pay http://127.0.0.1:8000/paymentLeading University, Sylhet"
+        send_mail("Successfully Registration",
+                  content,
+                  settings.EMAIL_HOST_USER,
+                  ["amadahmed1234678@gmail.com"]
+                  )
         return render(request, "home.html")
--- emadsite/settings.py
+++ emadsite/settings.py
@@ -132,5 +132,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "amadking1234678@gmail.com"
+EMAIL_HOST_PASSWORD = "amadking1234567"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 django_heroku.settings(locals())
